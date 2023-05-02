fd948d534d7e06137e924b41ef400340ddc37855
Pritam Das <dpritam28577@gmail.com>
2023-05-03 00:20:33 +0530

    the url split is done and the 3 test case are pass

--- src/crawl.test.js
+++ src/crawl.test.js
@@ -1,17 +1,31 @@
-const {noramlizeURL} = require('./crawl.js')
-const {test , expect} = require('@jest/globals')
+const { normalizeURL, getURLsFromHTML } = require('./crawl.js')
+const { test, expect } = require('@jest/globals')
 
-test('noramlizeURL ' , ()=>{
+test('noramlizeURL ', () => {
     const input = 'https://blog.boot.dev/path';
-    const actual = noramlizeURL(input);
+    const actual = normalizeURL(input);
     const expeceted = 'blog.boot.dev/path';
 
     expect(actual).toEqual(expeceted);
 })
-test('noramlizeURL strip protocol ' , ()=>{
+test('noramlizeURL strip protocol ', () => {
     const input = 'https://blog.boot.dev/path/';
-    const actual = noramlizeURL(input);
+    const actual = normalizeURL(input);
     const expeceted = 'blog.boot.dev/path';
+
+    expect(actual).toEqual(expeceted);
+})
+test('getURLsFromhtml', () => {
+    const inputHtml = `
+    <html>
+         <body>
+         <a href = "https://blog.boot.dev/">Boot.dev blog<a/>
+         </body>
+    </html>
+    `;
+    const inputUrl = "https://blog.boot.dev/"
+    const actual = getURLsFromHTML(inputHtml , inputUrl);
+    const expeceted = ["https://blog.boot.dev/"];
 
     expect(actual).toEqual(expeceted);
 })
